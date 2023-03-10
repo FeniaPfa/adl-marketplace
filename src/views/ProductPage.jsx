a13afe231fe3ccc97623df7dad1311e4d89b8d7b
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-03-10 00:42:36 -0300

    style: productPage

--- src/views/ProductPage.jsx
+++ src/views/ProductPage.jsx
@@ -10,10 +10,11 @@ import {
     Stack,
     Typography,
 } from '@mui/material';
+import { doc, getDoc } from 'firebase/firestore';
 import { getDownloadURL, ref } from 'firebase/storage';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { storage } from '../config/firebase';
+import { db, storage } from '../config/firebase';
 import { Main } from '../containers/Main';
 import { useGetData } from '../hooks/useGetData';
 import { formatNumber } from '../utils/utils.js';
@@ -21,13 +22,28 @@ import { formatNumber } from '../utils/utils.js';
 export const ProductPage = () => {
     const { id } = useParams();
     const [img, setImg] = useState();
+    const [userInfo, setUserInfo] = useState();
 
     const { products, setLoading, loading } = useGetData();
 
     const productData = products.find((item) => item.id === id);
-    console.log(productData);
+
+    const getUserInfo = async () => {
+        try {
+            if (productData) {
+                const userRef = doc(db, 'users', productData?.userId);
+                const docSnap = await getDoc(userRef);
+                const data = docSnap.data();
+                setUserInfo(data);
+            }
+        } catch (err) {
+            console.log({ err });
+        }
+    };
+
     const getImg = async () => {
         const imgRef = ref(storage, `products-img/${id}`);
+
         try {
             const url = await getDownloadURL(imgRef);
             setImg(url);
@@ -38,14 +54,16 @@ export const ProductPage = () => {
     };
     useEffect(() => {
         getImg();
-    }, []);
+
+        getUserInfo();
+    }, [productData]);
 
     const listStyle = {
         display: 'flex',
-        gap: {xs: "1rem",md :"5rem"},
+        gap: { xs: '1rem', md: '5rem' },
         color: '#455a64',
         fontWeight: 'bold',
-        flexWrap:{xs:"wrap",md:"noWrap"}
+        flexWrap: { xs: 'wrap', md: 'noWrap' },
     };
 
     if (!productData) {
@@ -54,75 +72,108 @@ export const ProductPage = () => {
 
     return (
         <Main>
-            <Container maxWidth="md">
-                <Card
+            <Card
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    padding: '.3rem 1.2rem 1.2rem',
+                }}>
+                <Box
+                    className="card-body"
                     sx={{
                         display: 'flex',
-                        flexDirection: 'column',
-                        padding: '.3rem 1.2rem 1.2rem',
                         gap: '1rem',
+                        alignItems: 'center',
+                        // flexWrap: "wrap",
+                        flexDirection: { xs: 'column', sm: 'row' },
                     }}>
-                    <Box sx={{ display: 'flex', gap:"1rem", alignItems:"center", flexWrap:"wrap" }}>
-                        <CardMedia
-                            component="img"
-                            title={productData?.sport}
-                            image={img}
-                            sx={{ width: {xs:"100%",md:'40%'}, objectFit: 'cover', aspectRatio:{md:"1/1"} }}
-                        />
+                    <CardMedia
+                        component="img"
+                        title={productData?.sport}
+                        image={img}
+                        sx={{
+                            width: { xs: '100%', sm: '40%' },
+                            objectFit: 'cover',
+                            aspectRatio: { sm: '1/1' },
+                        }}
+                    />
 
-                        <CardContent
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                gap: '.6rem',
-                                flexGrow: 1,
-                            }}>
-                            <Stack
-                                direction="row"
-                                justifyContent="space-between"
-                                sx={{ color: '#455a64' }}>
-                                <Typography variant="subtitle2" fontWeight="bold">
-                                    Clases de {productData?.sport}
-                                </Typography>
-                                <Typography variant="subtitle2" fontWeight="bold">
-                                    Viña del Mar
-                                </Typography>
-                            </Stack>
-                            <Typography variant="h4" fontWeight="bold">
-                                {productData?.dojo}
+                    <CardContent
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: '.6rem',
+                            flexGrow: 1,
+                        }}>
+                        <Stack
+                            direction="row"
+                            justifyContent="space-between"
+                            sx={{ color: '#455a64' }}>
+                            <Typography variant="overline" fontWeight="bold">
+                                Clases de {productData?.sport}
                             </Typography>
-                            <List disablePadding>
-                                <ListItem sx={listStyle}>
-                                    <span style={{ flexGrow: '1' }}>Dirección:</span>
-                                    <span>{productData.place}</span>
-                                </ListItem>
-                                <ListItem sx={listStyle}>
-                                    <span style={{ flexGrow: '1' }}>Nivel:</span>
-                                    <span>{productData.level}</span>
-                                </ListItem>
-                                <ListItem sx={listStyle}>
-                                    <span style={{ flexGrow: '1' }}>Edad:</span>
-                                    <span>{productData.age}</span>
-                                </ListItem>
-                            </List>
-                            <Typography variant="h4" fontWeight="bold" color="primary">
+                            <Typography variant="overline" fontWeight="bold">
+                                Viña del Mar
+                            </Typography>
+                        </Stack>
+                        <Typography
+                            variant="h4"
+                            fontWeight="bold"
+                            fontFamily="Kanit,sans-serif"
+                            letterSpacing=".3rem">
+                            {productData?.dojo}
+                        </Typography>
+                        <List disablePadding>
+                            <ListItem sx={listStyle}>
+                                <span style={{ flexGrow: '1' }}>Dirección:</span>
+                                <span>{productData.place}</span>
+                            </ListItem>
+                            <ListItem sx={listStyle}>
+                                <span style={{ flexGrow: '1' }}>Nivel:</span>
+                                <span>{productData.level}</span>
+                            </ListItem>
+                            <ListItem sx={listStyle}>
+                                <span style={{ flexGrow: '1' }}>Edad:</span>
+                                <span>{productData.age}</span>
+                            </ListItem>
+                        </List>
+                        <Box display="flex" gap="1rem" flexDirection="row" alignItems="baseline">
+                            <Typography
+                                variant="h4"
+                                fontWeight="bold"
+                                color="primary"
+                                letterSpacing=".3rem"
+                                lineHeight="1.5">
                                 $ {formatNumber(productData?.price)}
                             </Typography>
-                            <Stack direction="row" justifyContent="space-around">
-                                
+                            <Typography
+                                variant="subtitle2"
+                                letterSpacing=".1rem"
+                                textTransform="uppercase">
+                                x mes
+                            </Typography>
+                        </Box>
+                        <Stack direction="row" justifyContent="space-around">
+                            <Button variant="outlined">Guardar en favoritos</Button>
                             <Button variant="contained">Añadir al carrito</Button>
-                            <Button variant="contained">Guardar en favoritos</Button>
-                            </Stack>
-                        </CardContent>
-                    </Box>
-                    {/* <Typography>{productData?.desc}</Typography> */}
+                        </Stack>
+                    </CardContent>
+                </Box>
+                <Box className="card-bottom">
+                    <Typography variant="overline" sx={{ lineHeight: '0' }}>
+                        Por:
+                        <b>
+                            {' '}
+                            {userInfo?.name} {userInfo?.apellido}
+                        </b>
+                    </Typography>
                     <Typography>
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta inventore
                         provident velit totam nihil sequi fugiat quia qui dolorem impedit maxime,
                         modi quos maiores rerum repellat numquam dolore libero suscipit!
                     </Typography>
-                </Card>
-            </Container>
+                </Box>
+            </Card>
         </Main>
     );
 };
