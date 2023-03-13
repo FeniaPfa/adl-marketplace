3a4664b4c7e498ba1977843ab375613287acc573
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-03-13 01:14:14 -0300

    style:Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { SvgIcon, Typography } from '@mui/material';
+import { Stack, SvgIcon, Typography } from '@mui/material';
 import heroimg from '../assets/kimono.png';
 
 import logo from '/logo.svg';
@@ -7,16 +7,35 @@ export const Hero = () => {
     return (
         <div className="hero">
             <div className="hero__text">
-                <h1 className="hero__title">TATAMI</h1>
-                <h2 className="hero__cta">
-                    Supera los desafíos y alcanza <span className='red'>tu máximo potencial</span> físico y mental
-                </h2>
-                <p className="hero__subtitle">
-                Encuentra y publica tus clases de artes marciales en un solo lugar.
-                </p>
-                <p className="hero__subtitle">
-                    ¡Descubre la fuerza interior y la disciplina de las artes marciales con TATAMI!
-                </p>
+                <Typography
+                    variant="h1"
+                    fontFamily="Kanit,sans-serif"
+                    fontWeight="bold"
+                    fontSize="4rem"
+                    className="hero__title">
+                    TATAMI
+                </Typography>
+                <Typography
+                    fontSize="3.2rem"
+                    fontWeight="bold"
+                    lineHeight="1.2"
+                    fontFamily="Kanit,sans-serif"
+                    fontStyle="italic"
+                    className="hero__cta">
+                    Supera los desafíos y alcanza <span className="red">tu máximo potencial</span>{' '}
+                    físico y mental
+                </Typography>
+                <div>
+                    <Stack gap="1rem">
+                        <Typography fontSize="1.5rem" fontWeight="bold" className="hero__subtitle">
+                            ¡Descubre la fuerza interior y la disciplina de las artes marciales con
+                            TATAMI!
+                        </Typography>
+                        <Typography fontSize="1.5rem" fontWeight="bold" className="hero__subtitle">
+                            Encuentra y publica tus clases de artes marciales en un solo lugar.
+                        </Typography>
+                    </Stack>
+                </div>
             </div>
             <img className="hero__img" src={heroimg} />
         </div>
