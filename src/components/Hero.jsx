0656004dc399d0be153d3079effa2287286f8dfa
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-03-15 00:37:45 -0300

    style: responsive Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,44 +1,56 @@
-import { Button, Stack, Typography } from '@mui/material';
+import { Box, Button, Stack, Typography } from '@mui/material';
 import heroimg from '../assets/hero.png';
-
-import logo from '/logo.svg';
 
 export const Hero = () => {
     return (
-        <div className="hero">
-            <div className="hero__text">
-                {/* <Typography
-                    variant="h1"
-                    fontFamily="Kanit,sans-serif"
-                    fontWeight="bold"
-                    fontSize="4rem"
-                    className="hero__title">
-                    TATAMI
-                </Typography> */}
+        <Box
+            className="hero"
+            sx={{
+                position:"relative",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'flex-start',
+                flexDirection: { xs: 'column', md: 'row' },
+                height:"80vh",
+                '> img': { display: { xs: 'none', md: 'initial' }, height:{md:"60%",lg:"100%"}, alignSelf:"flex-end" }
+            }}>
+            <Box
+                className="hero__text"
+                sx={{
+                    width: { xs: '80%', sm: '70%', md: '50%',lg:"40%" },
+                }}>
                 <Typography
-                    fontSize="3.2rem"
                     fontWeight="bold"
                     lineHeight="1.2"
                     fontFamily="Kanit,sans-serif"
                     fontStyle="italic"
-                    className="hero__cta">
+                    className="hero__cta"
+                    sx={{
+                        fontSize: { xs: '2rem', sm: '3rem', md: '2.5rem', lg:"3.2rem" },
+                    }}>
                     Supera los desafíos y alcanza <span className="red">tu máximo potencial</span>{' '}
                     físico y mental
                 </Typography>
                 <div>
-                    <Stack gap="1.2rem" alignItems="flex-start">
-                        <Typography fontSize="1.5rem" fontWeight="bold" className="hero__subtitle">
+                    <Stack
+                        gap="1.2rem"
+                        alignItems="flex-start"
+                        sx={{ '> p': { fontSize: { xs: '1.2rem', sm: '1.3rem', md: '1.5rem' } } }}>
+                        <Typography fontWeight="bold" className="hero__subtitle">
                             ¡Descubre la fuerza interior y la disciplina de las artes marciales con
                             TATAMI!
                         </Typography>
-                        <Typography fontSize="1.5rem" fontWeight="bold" className="hero__subtitle">
+                        <Typography fontWeight="bold" className="hero__subtitle">
                             Encuentra y publica clases de artes marciales en un solo lugar.
                         </Typography>
-                        <Button variant='contained' size="large" sx={{padding:"1rem 2rem"}}>Buscar Clases</Button>
+                        <Button variant="contained" size="large" sx={{ padding: '1rem 2rem' }}>
+                            Buscar Clases
+                        </Button>
                     </Stack>
                 </div>
-            </div>
+            </Box>
+
             <img className="hero__img" src={heroimg} />
-        </div>
+        </Box>
     );
 };
