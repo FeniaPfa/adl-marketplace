39d6b09956ab2196c75260d086b6b25ab66e9790
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-03-14 21:33:16 -0300

    feat: filters

---
+++ src/components/Filters.jsx
@@ -0,0 +1,66 @@
+import { Container, MenuItem, TextField } from '@mui/material';
+
+export const Filters = ({ products, setFilteredList }) => {
+    const handleSort = (type) => {
+        let sortedList = [...products];
+        if (type === 'cheap') {
+            sortedList.sort((a, b) => b.price - a.price);
+            setFilteredList(sortedList);
+        }
+        if (type === 'expensive') {
+            sortedList.sort((a, b) => a.price - b.price);
+            setFilteredList(sortedList);
+        }
+    };
+
+    const handleSearch = (e) => {
+        const query = e.target.value;
+        setFilteredList(
+            products.filter((item) =>
+                item.sport.toLowerCase().includes(query.toLowerCase())
+            )
+        );
+    };
+    const handleSearch2 = (e) => {
+        const query = e.target.value;
+        setFilteredList(
+            products.filter((item) =>
+                item.city.toLowerCase().includes(query.toLowerCase())
+            )
+        );
+    };
+
+    return (
+        <>
+            <Container
+                maxWidth="md"
+                sx={{ display: 'flex', flexDirection: 'row', gap: '3rem', marginBottom: '2rem'}}>
+                <TextField
+                    value={'x'}
+                    select
+                    fullWidth
+                    label="Ordenar por"
+                    onChange={(e) => handleSort(e.target.value)}>
+                    <MenuItem disabled value="x">
+                        <em>Ordenar</em>
+                    </MenuItem>
+                    <MenuItem value="cheap">Precio Ascendente</MenuItem>
+                    <MenuItem value="expensive">Precio Descendiente</MenuItem>
+                </TextField>
+
+                <TextField
+                    fullWidth
+                    label="Buscar por disciplina"
+                    placeholder="Karate..."
+                    onChange={handleSearch}
+                />
+                <TextField
+                    fullWidth
+                    label="Buscar por city"
+                    placeholder="Karate..."
+                    onChange={handleSearch2}
+                />
+            </Container>
+        </>
+    );
+};
--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -3,16 +3,23 @@ import { ProductCard } from '../components/ProductCard';
 import { CardSkeleton } from '../components/CardSkeleton';
 import { Stack } from '@mui/material';
 import { Main } from '../containers/Main';
+import { Filters } from '../components/Filters';
+import { useEffect, useState } from 'react';
 
 export const Products = () => {
-    const { products, loading } = useGetProducts();
+    const { products, loading, setProducts} = useGetProducts();
+    const [filteredList, setFilteredList] = useState([])
+
+    useEffect(() => {
+        setFilteredList(products)
+    },[products])
     return (
         <Main>
-
+            <Filters products={products} setFilteredList={setFilteredList}/>
         <Stack direction="row" gap="2rem" flexWrap="wrap">
             {loading
                 ? Array.from(new Array(6)).map((item, index) => <CardSkeleton key={index} />)
-                : products.map((item) => <ProductCard key={item.id} product={item} />)}
+                : filteredList.map((item) => <ProductCard key={item.id} product={item} />)}
         </Stack>
                 </Main>
     );
