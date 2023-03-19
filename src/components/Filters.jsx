adf6e9bbd4cc2c75bc72552f0b8c5fecccdb9617
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-03-19 16:19:53 -0300

    style: ProductPage Filters component

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,12 @@
-import { Container, MenuItem, TextField } from '@mui/material';
+import { Container, InputAdornment, MenuItem, TextField } from '@mui/material';
 import { useEffect, useState } from 'react';
+import SortIcon from '@mui/icons-material/Sort';
+import SportsMartialArtsIcon from '@mui/icons-material/SportsMartialArts';
+import PlaceIcon from '@mui/icons-material/Place';
 
 export const Filters = ({ products, setFilteredList, filteredList }) => {
-
-    const [city, setCity] = useState("")
-    const [sport, setSport] = useState("")
+    const [city, setCity] = useState('');
+    const [sport, setSport] = useState('');
 
     const handleSort = (type) => {
         let sortedList = [...filteredList];
@@ -19,47 +21,75 @@ export const Filters = ({ products, setFilteredList, filteredList }) => {
     };
 
     useEffect(() => {
-        let newData = [...products]
-        if(sport) {
-            newData = newData.filter(item => item.sport.toLowerCase().includes(sport.toLowerCase()))
+        let newData = [...products];
+        if (sport) {
+            newData = newData.filter((item) =>
+                item.sport.toLowerCase().includes(sport.toLowerCase())
+            );
         }
-        if(city){
-            newData= newData.filter(item => item.city.toLowerCase().includes(city.toLowerCase()))
+        if (city) {
+            newData = newData.filter((item) =>
+                item.city.toLowerCase().includes(city.toLowerCase())
+            );
         }
-        setFilteredList(newData)
-    },[sport, city])
+        setFilteredList(newData);
+    }, [sport, city]);
 
     return (
+        <Container
+            maxWidth="md"
+            sx={{
+                display: 'flex',
+                flexDirection: { xs: 'column', sm: 'row' },
+                gap: '2rem',
+                marginBottom: '2rem',
+            }}>
+            <TextField
+                value={'x'}
+                select
+                fullWidth
+                label="Ordenar por"
+                onChange={(e) => handleSort(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SortIcon />
+                        </InputAdornment>
+                    ),
+                }}>
+                <MenuItem disabled value="x">
+                    <em>Ordenar</em>
+                </MenuItem>
+                <MenuItem value="cheap">Precio Descendiente</MenuItem>
+                <MenuItem value="expensive">Precio Ascendente</MenuItem>
+            </TextField>
 
-            <Container
-                maxWidth="md"
-                sx={{ display: 'flex', flexDirection: 'row', gap: '3rem', marginBottom: '2rem' }}>
-                <TextField
-                    value={'x'}
-                    select
-                    fullWidth
-                    label="Ordenar por"
-                    onChange={(e) => handleSort(e.target.value)}>
-                    <MenuItem disabled value="x">
-                        <em>Ordenar</em>
-                    </MenuItem>
-                    <MenuItem value="cheap">Precio Descendiente</MenuItem>
-                    <MenuItem value="expensive">Precio Ascendente</MenuItem>
-                </TextField>
-
-                <TextField
-                    fullWidth
-                    label="Buscar por disciplina"
-                    placeholder="Karate..."
-                    onChange={(e) => setSport(e.target.value)}
-                />
-                <TextField
-                    fullWidth
-                    label="Buscar por ciudad"
-                    placeholder="Viña del Mar..."
-                    onChange={(e) => setCity(e.target.value)}
-                />
-            </Container>
-
+            <TextField
+                fullWidth
+                label="Buscar por disciplina"
+                placeholder="Karate..."
+                onChange={(e) => setSport(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SportsMartialArtsIcon />
+                        </InputAdornment>
+                    ),
+                }}
+            />
+            <TextField
+                fullWidth
+                label="Buscar por ciudad"
+                placeholder="Viña del Mar..."
+                onChange={(e) => setCity(e.target.value)}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <PlaceIcon />
+                        </InputAdornment>
+                    ),
+                }}
+            />
+        </Container>
     );
 };
