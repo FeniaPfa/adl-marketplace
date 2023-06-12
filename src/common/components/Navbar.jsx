d97283e62b1674b9b64c11845e8b5f32b8e31349
Fernanda Aguilar Pfannschmidt <fa.aguilarpfa@gmail.com>
2023-06-11 21:45:26 -0400

    fix: :bug: navbar import

--- src/common/components/Navbar.jsx
+++ src/common/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 import { logOut } from '../../config/firebase';
